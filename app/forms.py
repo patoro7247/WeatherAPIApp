e792b404dbb26db3d69ef3d0fbc129457f1c3af8
Patrick Harding <patrickhardingr@gmail.com>
2021-06-20 11:15:17 -0700

    completed form registration to add users to database

--- app/forms.py
+++ app/forms.py
@@ -13,3 +13,14 @@ class PostForm(FlaskForm):
 	ReceiveNotifications = BooleanField('Recieve Post Notifications')
 
 
+class RegistrationForm(FlaskForm):
+	username = EmailField('Email', validators = [InputRequired(message="Email required")])
+	city = StringField('City', validators = [InputRequired(message="City required")])
+	state = StringField('State', validators = [InputRequired(message="State required")])
+	temp = IntegerField('Temp', validators = [InputRequired(message="Temperature required")])
+	submit = SubmitField('Submit')
+	
+	def validate_email(self, email):
+		user = User.query.filter_by(username=username.data).first()
+		if user is not None:
+			raise ValidationError('That email is already being used.')
--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,20 @@
 from flask import render_template, flash, redirect, url_for
-from app import app
-from app.forms import PostForm
+from app import app, db
+from app.models import User
+from app.forms import RegistrationForm
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-	form = PostForm()
+	form = RegistrationForm()
 	if form.validate_on_submit():
-		flash('Submission Requested for:\nCity: {}, State: {}'.format(form.city.data, form.state.data))
+		#user = User.query.filter_by(username=form.email.data).first()
+		location = form.city.data + ', '+form.state.data
+
+		user = User(username=form.username.data, address=location, desired_temperature=form.temp.data)
+		db.session.add(user)
+		db.session.commit()
+		flash('We\'ll now send you an email whenever the temperature high in your city is at or above your desired temp')
 		return redirect(url_for('confirmation'))
 	return render_template('index.html', form=form)
 
